dcc2f38948e29a76ac37d73dfe5d638eb86e60f7
@parou.char <curlycode@outlook.com>
2024-05-13 00:16:29 +0300

    fixing layout and adding instagram icon

--- src/Components/Layouts/TopBar.js
+++ src/Components/Layouts/TopBar.js
@@ -9,18 +9,18 @@ function TopBar() {
     <div className="w-4/5 mx-10  mt-5 mb-9  h-10 md:mx-auto">
       <div className="flex flex-row items-center justify-around">
         <div className="flex flex-col p-0 sm:flex-row justify-start items-center  p-2 font-semibold">
-          <div className="flex flex-row items-center mt-0 text-md md:text-base w-40">
+          <div className="flex flex-row items-center mt-0 text-md md:text-md w-2/3">
             <FontAwesomeIcon icon={faPhone} />
-            <h3 className="mx-1 text-blue-900  ">(+30) 6977500987</h3>
+            <h3 className="mx-1  text-base text-blue-900  ">(+30) 6977500987</h3>
           </div>
 
-          <div className="flex flex-row items-center mt-0 ml-14 mx-8 text-md md:text-base">
+          <div className="flex flex-row items-center mt-0 ml-14 mx-10 text-base md:text-md">
             <FontAwesomeIcon icon={faEnvelope} />
-            <h3 className="mx-1 text-md text-blue-900">harlawoffice@gmail.com</h3>
+            <h3 className="mx-1 text-base text-blue-900">harlawoffice@gmail.com</h3>
           </div>
-          <div className="flex flex-row items-center mr-5 mt-0 ml-0 mx-0 text-xl md:text-xl">
+          <div className="flex flex-row items-center  mt-0 ml-0 mx-9 text-xl md:text-xl">
            <FontAwesomeIcon icon={faInstagram} />
-          <a className="text-md" href="https://www.instagram.com/haralambidis_nick">   <h3  className="mx-1 text-md text-blue-900">haralambidis_nick</h3></a>
+          <a className="text-md" href="https://www.instagram.com/haralambidis_nick">   <h3  className="mx-1 text-base text-blue-900">haralambidis_nick</h3></a>
           </div>
         </div>
    
